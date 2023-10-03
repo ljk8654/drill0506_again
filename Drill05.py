a6fff72a826beb65ce9b3432386ca9ea35e595ac
ljk8654 <132908803+ljk8654@users.noreply.github.com>
2023-10-04 01:59:39 +0900

    기본 refactory 완료 (함수로 구조화시킴)

--- Drill05.py
+++ Drill05.py
@@ -1,10 +1,12 @@
 from pico2d import *
 
 TUK_WIDTH, TUK_HEIGHT = 1280, 1024
-open_canvas(TUK_WIDTH, TUK_HEIGHT)
 
-TUK_ground = load_image('TUK_GROUND.png')
-character = load_image('animation_sheet.png')
+
+def load_resource():
+    global TUK_ground, character
+    TUK_ground = load_image('TUK_GROUND.png')
+    character = load_image('animation_sheet.png')
 
 
 def handle_events():
@@ -21,23 +23,33 @@ def handle_events():
     pass
 
 
+def reset_world():
+    global running, x, y, frame
+    running = True
+    x, y = TUK_WIDTH // 2, TUK_HEIGHT // 2
+    frame = 0
 
-running = True
-x, y = TUK_WIDTH // 2, TUK_HEIGHT // 2
-frame = 0
-hide_cursor()
 
-while running:
+def render_world():
     clear_canvas()
     TUK_ground.draw(TUK_WIDTH // 2, TUK_HEIGHT // 2)
     character.clip_draw(frame * 100, 100 * 1, 100, 100, x, y)
     update_canvas()
+
+
+def udate_world():
+    global frame
     frame = (frame + 1) % 8
 
-    handle_events()
+
+open_canvas(TUK_WIDTH, TUK_HEIGHT)
+hide_cursor()
+load_resource()
+reset_world()
+
+while running:
+    render_world()  # 월드의 현재 내용을 그린다
+    handle_events()  # 사용자의 입력을 받아들인다.
+    udate_world()  # 원드 안의 객체들의 상호작용을 계산하고 업데이트를 한다
 
 close_canvas()
-
-
-
-
